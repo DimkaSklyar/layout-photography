c82bec49ab8eb078f2d3db22d333761466319d8a
DimkaSklyar <DimkaSklyar@mail.ru>
2018-03-20 23:49:42 +0300

    edit header

---
+++ src/js/common.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+
+var touch = $('#touch-menu');
+    var menu = $('.nav');
+ 
+    $(touch).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 760 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+
+
+});
